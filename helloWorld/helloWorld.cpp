03f43deb4924b2e9a2d3074823ffc36d55278049
Izaliya Izawgawd <izaliyaizawgawd@yahoo.com>
2024-05-17 04:44:12 +0800

    Ye

--- helloWorld/helloWorld.cpp
+++ helloWorld/helloWorld.cpp
@@ -4,7 +4,7 @@
 // main funtion is the entry point of the program.
 int main() {
 	// std means standard, cout means character output, << means output or left shift operator,
-	std::cout << "Why am I stuck here" << std::endl; // this will print the text to the console.
+	std::cout << "Why am I stuck here" << std::endl; // this will print the text to the console, endl (end line) is used to move to the next line.
 	std::cout << "Just to suffer";
 		return 0; // return 0 means the program has executed successfully.
 }
