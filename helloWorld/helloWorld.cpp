6d4c73dac6ea19c167304071260e77a9ad0d90e0
Izaliya Izawgawd <izaliyaizawgawd@yahoo.com>
2024-05-17 04:48:31 +0800

    Ye

--- helloWorld/helloWorld.cpp
+++ helloWorld/helloWorld.cpp
@@ -6,5 +6,6 @@ int main() {
 	// std means standard, cout means character output, << means output or left shift operator,
 	std::cout << "Why am I stuck here" << std::endl; // this will print the text to the console, endl (end line) is used to move to the next line.
 	std::cout << "Just to suffer";
+	// std::'.\n' is used to move to the next line as well and better performance than endl.
 		return 0; // return 0 means the program has executed successfully.
 }
