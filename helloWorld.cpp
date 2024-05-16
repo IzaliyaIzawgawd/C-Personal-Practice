219e84f88f471cb8b4ffa9070a91186bff979772
Izaliya Izawgawd <izaliyaizawgawd@yahoo.com>
2024-05-17 03:59:15 +0800

    Hello World?

---
+++ helloWorld.cpp
@@ -0,0 +1,9 @@
+//iostream is a header file that contains functions for basic input/output.
+#include <iostream>
+
+// main funtion is the entry point of the program.
+int main() {
+	// std means standard, cout means character output, << means output or left shift operator,
+	std::cout << "Why am I stuck here"; // this will print the text to the console.
+		return 0; // return 0 means the program has executed successfully.
+}
