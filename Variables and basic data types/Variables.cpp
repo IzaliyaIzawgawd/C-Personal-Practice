f8f110e6c4fa2899065690a7949a6b514fdd274e
Izaliya Izawgawd <izaliyaizawgawd@yahoo.com>
2024-06-06 00:55:43 +0800

    A

--- Variables and basic data types/Variables.cpp
+++ Variables and basic data types/Variables.cpp
@@ -4,9 +4,11 @@ int main() { //integer main function
 	
 	int x = 5; // declaration
 	int y = 6;
+	int sum = x + y;
 
 	std::cout << x << '\n'; // output while \n is used to move to the next line
 	std::cout << y << '\n';
+	std::cout << sum << '\n';
 
 	return 0;
 }
