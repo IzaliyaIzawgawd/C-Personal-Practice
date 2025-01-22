31543a0c2610b6bdecd0dc02eb6b57dd5fffe590
Izaliya Izawgawd <izaliyaizawgawd@yahoo.com>
2025-01-22 14:23:41 +0800

    A

--- Variables and basic data types/Variables.cpp
+++ Variables and basic data types/Variables.cpp
@@ -18,5 +18,35 @@ int main() { //integer main function
 	std::cout << "year = " << year << '\n';
 	std::cout << "days = " << days << '\n';
 
+	//double is numbers with decimals
+	double price = 10.99;
+	double gpa = 2.5;
+	double temperature = 30.5;
+
+	std::cout << "price = " << price << '\n';
+	std::cout << "gpa = " << gpa << '\n';
+	std::cout << "temperature = " << temperature << '\n';
+
+	//char is single character only
+	char grade = 'A';
+	char initial = 'B';
+	
+	std::cout << "Grade = " << grade << '\n';
+	std::cout << "Initial = " << initial << '\n';
+
+	//boolean is either true or false
+	bool student = true;
+	bool power = true;
+	bool forSale = false;
+
+	std::cout << "Student = " << student << '\n';
+	std::cout << "Power = " << power << '\n';
+	std::cout << "For Sale = " << forSale << '\n';
+
+	//string, objects that represent a sequence of texts
+	std::string name = "Izaliya Izawgawd";
+
+	std::cout << "Name = " << name << '\n';
+
 	return 0;
 }
