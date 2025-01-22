f275fdfa772fdc60a9ab32a017fa69ce0d9b5ab8
Izaliya Izawgawd <izaliyaizawgawd@yahoo.com>
2025-01-22 13:21:54 +0800

    a

--- Variables and basic data types/Variables.cpp
+++ Variables and basic data types/Variables.cpp
@@ -9,6 +9,14 @@ int main() { //integer main function
 	std::cout << x << '\n'; // output while \n is used to move to the next line
 	std::cout << y << '\n';
 	std::cout << sum << '\n';
+	//More math stuff
+	int age = 22;
+	int year = 2025;
+	int days = 201.5;
+
+	std::cout << "age = "; << age << '\n';
+	std::cout << "year = " << year << "\n';
+	std::cout << "days = " << days << '\n';
 
 	return 0;
 }
