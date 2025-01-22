ec9c705e6b1d0e20f218e21713428eb8b0a9e015
Izaliya Izawgawd <izaliyaizawgawd@yahoo.com>
2025-01-22 14:49:55 +0800

    A

---
+++ Const/Const.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main() {
+	//constants are variables whose value cannot be changed once it's been initialized
+	const double pi = 3.14159;
+	pi = 42069
+	double radius = 10;
+	double circumference = 2 * pi * radius;
+	
+	std::cout << "Circumference = " << circumference << "cm" << '\n';
+	
+	const int Light_Speed = 299792458;
+	const int Width = 1366;
+	const int Height = 768;
+	
+	return 0;
+}
