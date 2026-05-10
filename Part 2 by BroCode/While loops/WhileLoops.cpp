8373442355b6ebc8720ef2405e985efd9e91c8a7
Izaliya Izawgawd <izaliyaizawgawd@yahoo.com>
2026-05-10 23:25:48 +0800

    A

---
+++ Part 2 by BroCode/While loops/WhileLoops.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main() {
+
+	std::string name;
+
+	//while(name.empty()){
+	//if(name.empty()) {
+	    while(1==1){
+			std::cout << "HELP!";
+		//std::cout << "Enter your name: ";
+		//std::getline(std::cin, name);
+	}
+
+	std::cout << "Hello " << name;
+
+	
+	return 0;
+
+}
